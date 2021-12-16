aaa72b378a0a7982d4c985c0e2dd24637e11f1a5
Mindy Jones <38539902+mexgirlmindy@users.noreply.github.com>
2021-12-16 14:25:38 -0600

    Update BudgetingCalculator.py
    Added a gui

--- BudgetingCalculator.py
+++ BudgetingCalculator.py
@@ -1,41 +1,35 @@
 
   #Simple calculator for converting hourly pay to an annual salary. I haven't been able to find an online calculator that does all this.
+  
+import tkinter as tk
+from math import *
 
-def Gen_budget(budget):
-    print ("************************************************ ")
-    print ("General budget ")
-    print ("************************************************ ")
-    monthly_pay = budget/12
-    print ("Monlthy Pay:                "  + str(round(monthly_pay,2)) )
-    print ("Maximum Rent:               "  + str(round(monthly_pay*.30,2)) )
-    print ("Maximum Utilities:          "  + str(round(monthly_pay*.10,2)) )
-    print ("Maximum Transportion Cost:  "  + str(round(monthly_pay*.20,2)) )
-    print ("Maximum Spending Money:     "  + str(round(monthly_pay*.10,2)) )
-    print ("General Savings:            "  + str(round(monthly_pay*.15,2)) )
-    print ("Student Loans:              "  + str(round(monthly_pay*.05,2)) )
-    print ("Groceries:                  "  + str(round(monthly_pay*.10,2)) )
+def budget(Monthlypay):
+    tk.Label(text='Monthly Take Home Pay ' + str(Monthlypay)).pack()
+    tk.Label(text='Hourly Pay ' + str(round(Monthlypay/160,2))).pack()
+    tk.Label(font=16, text='Budget ').pack()
+    tk.Label( text='Monthly Pay: ' + str(Monthlypay)).pack()
+    tk.Label( text='Maximum Rent: ' + str(round(Monthlypay*.3,2))).pack()
+    tk.Label( text='Monthly Utilities: ' + str(round(Monthlypay*.1,2))).pack()
+    tk.Label(text='Monthly Transportation Cost : ' + str(round(Monthlypay * .2, 2))).pack()
+    tk.Label(text='Monthly Spending Money: ' + str(round(Monthlypay * .1, 2))).pack()
+    tk.Label(text='Monthly Student Loans: ' + str(round(Monthlypay * .15, 2))).pack()
+    tk.Label(text='Monthly Groceries: ' + str(round(Monthlypay * .05, 2))).pack()
 
-def Gen_mortage(pay):
-    print ("************************************************ ")
-    print ("General Mortage Calculator ")
-    print ("************************************************ ")
-    Home_Cost = pay * 4
-    print ("House Price Estimate "  + str(round(Home_Cost,2)) )
-    print ("Note: For a 30 year mortage, house payment is less than rent")
+def calculate(event):
+    Salary = int((entry.get()))
+    Monthlypay = Salary*.85*.083 # Estimated taxes
+    res.configure(  text="Maximum House Mortgage: " + str(4*Salary))
+    budget(Monthlypay)
 
-def Gen_pay(rate):
-    annual_hours = 2080.00
-    annual_rate = rate*annual_hours
-    print ("************************************************ ")
-    print ("Annual Salary")
-    print ("************************************************ ")
-    print ("Annual pre-tax pay :        "  + str(annual_rate) )
-    average_take_home_pay = .85
-    after_taxes = float(annual_rate* average_take_home_pay)
-    print ("Annual post-tax pay :       "  + str(after_taxes))
-    Gen_budget(annual_rate)
-    Gen_mortage(annual_rate)
 
-print('What is the hourly rate?')
-hourly_rate = int(input())
-Gen_pay(hourly_rate)
+w = tk.Tk()
+w.title("Mindy's Budget Calculator")
+tk.Label(w, text="Enter Annual Salary:", font=16).pack()
+#tk.Label(w, text="                                              ", font=16).pack()
+entry = tk.Entry(w)
+entry.bind("<Return>", calculate)
+entry.pack()
+res = tk.Label(w)
+res.pack()
+w.mainloop()
